bc222da40b3e47b614ddd0b2adc31d5d9c0aeb22
Ch Faizan Mustansar <chaudhry.mustansar@iem.fraunhofer.de>
2021-01-02 19:42:22 +0100

    Fizzbuzz problem of HackerRank

--- src/main/java/com/faizan/speed/programming/Application.java
+++ src/main/java/com/faizan/speed/programming/Application.java
@@ -1,7 +1,10 @@
 package com.faizan.speed.programming;
 
+import com.faizan.speed.programming.fizzbuzz.FizzBuzz;
+import com.faizan.speed.programming.twosum.TwoSum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +13,17 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public void executionFunction () {
+		/*******************************
+		 * HackerRank FizzBuzz Problem
+		 * Complexity: VERY EASY
+		 *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
+
+		FizzBuzz fb = new FizzBuzz();
+		fb.execute();
+
+	}
 }
+
+
---
+++ src/main/java/com/faizan/speed/programming/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,29 @@
+package com.faizan.speed.programming.fizzbuzz;
+
+/**
+ * HackerRank question
+ */
+public class FizzBuzz {
+
+    public void execute() {
+        int i=1;
+        String toPrint = null;
+        while(i<=100) {
+            if(i%3 == 0 && i%5 == 0) {
+                toPrint="FizzBuzz";
+            } else if(i%3 == 0) {
+                toPrint="Fizz";
+            }  else if(i%5 == 0) {
+                toPrint="Buzz";
+            } else {
+                toPrint=Integer.toString(i);
+            }
+            i++;
+            System.out.println(toPrint);
+        }
+    }
+
+}
+
+
+
